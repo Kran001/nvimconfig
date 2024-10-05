8a3730f62ab60a033edecf7f116ec5e1b6e8df8f
Alexander Pogorelskiy <apogorelskiy@ozon.ru>
2024-10-05 13:29:23 +0300

    Fix tabs

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 bl.setup{
   options = {
     mode = "tabs",
-	diagnostics = "nvim_lsp",
+    diagnostics = "nvim_lsp",
     always_show_bufferline = true,
     show_buffer_close_icons = true,
     show_close_icon = false,
